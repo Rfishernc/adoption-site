c9465001105380c751b8b5da3b183dd8664d93b3
RichFisher <rich.fishernc@gmail.com>
2018-10-03 19:19:00 -0500

    printing pets to dom works

--- js/comp/petComp.js
+++ js/comp/petComp.js
@@ -4,4 +4,28 @@ function setPets(data) {
     pets = data;
 }
 
-export {setPets};
+function getPet() {
+    return pets;
+}
+
+function writeToDom(string) {
+    document.getElementById('petDiv').innerHTML = string;
+}
+
+function writePets(animal) {
+    let string = '';
+    console.log(animal);
+    for(let i = 0; i < animal.length; i++) {
+    string +=   `<div class="card col-4">`
+    string +=       `<div class="card-header">${animal[i].name}</div>`
+    string +=       `<div class="card-body">` 
+    string +=           `<img class="card-img-top" src=${animal[i].imageUrl} alt=${animal[i].name}>`
+    string +=           `<p class="card-text">${animal[i].specialSkill}</p>`
+    string +=           `<div class="card-footer ${animal[i].color}">${animal[i].type}</div>`
+    string +=      `</div>`
+    string +=           `</div>`;
+    }
+    writeToDom(string);
+}
+
+export {setPets, writePets, writeToDom, getPet};
